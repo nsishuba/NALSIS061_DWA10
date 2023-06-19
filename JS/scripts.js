d23c1c952f0244602332289936020bf7b75bf16e
nsishuba <nalisishuba@gmail.com>
2023-06-19 18:49:50 +0200

    Added reset button.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,15 +1,17 @@
 const MAX_NUMBER = 15;
 const MIN_NUMBER = -5;
-const STEP_AMOUNT = 5;
+const STEP_AMOUNT = 1;
 
 const number = document.querySelector('[data-key="number"]');
 const subtract = document.querySelector('[data-key="subtract"]');
 const add = document.querySelector('[data-key="add"]');
+const reset = document.querySelector('[data-key="reset"]');
+const confirm = document.querySelector('[data-key="confirm"]');
+
 
 const subtractHandler = () => {
   const newValue = parseInt(number.value) - STEP_AMOUNT;
   number.value = newValue;
-
   if (add.disabled === true) {
     add.disabled = false;
   }
@@ -21,7 +23,6 @@ const subtractHandler = () => {
 const addHandler = () => {
   const newValue = parseInt(number.value) + STEP_AMOUNT;
   number.value = newValue;
-
   if (subtract.disabled === true) {
     subtract.disabled = false;
   }
@@ -30,6 +31,12 @@ const addHandler = () => {
     add.disabled = true;
   }
 };
+const resetHandler = () => {
+  const newValue = 0;
+  number.value = newValue;
+  confirm.show();
 
+}
 subtract.addEventListener("click", subtractHandler);
 add.addEventListener("click", addHandler);
+reset.addEventListener("click", resetHandler);
